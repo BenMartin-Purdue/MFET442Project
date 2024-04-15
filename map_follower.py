302b5c0e66bce3b85c30d399a5910ccd54184cb5
Zachary <139710414+ZachWilson03@users.noreply.github.com>
2024-04-15 12:12:49 -0400

    Updates to control_loop()
    - Added PID controllers to initialization of MapFollower
    - Added PID controller error calculation in control_loop()
    - Updated commenting

--- map_follower.py
+++ map_follower.py
@@ -15,13 +15,15 @@ import rospy
 import geometry_msgs.msg
 import tf
 import math
-from MFET44200_Lab06_Steering import calculate_linear_dist, calculate_perpendicular_dist, calculate_steering_angle, calculate_waypoint_angle
+from simple_pid import PID
+from MFET44200_Lab06_Utils import calculate_linear_dist
+from MFET44200_Lab06_Steering import calculate_perpendicular_dist, calculate_steering_angle, calculate_waypoint_angle
 
 class MapFollower:
     def __init__(self):
-    #############################################################################################
-    # 
-    #############################################################################################    
+        #############################################################################################
+        # General initalization of the MapFollower class
+        #############################################################################################    
         self.waypoints = waypoint()
         self.index = 1
         self.desired_waypoint = self.waypoints.pose_arr[self.index]
@@ -30,12 +32,31 @@ class MapFollower:
         rospy.Subscriber("/amcl_pose", geometry_msgs.msg.PoseWithCovarianceStamped, self.update_current_pose)
         self.current_pose = []
 
+        # PID controller
+        
+        # control parameters
+        accel_Kp = 0
+        accel_Ki = 0
+        accel_Kd = 0
+
+        steer_Kp = 0
+        steer_Ki = 0
+        steer_Kd = 0
+
+        self.set_vel = 600
+        self.steering_vel = 20
+        # Control parameters
+
+        self.accel_PID = PID(accel_Kp, accel_Ki, accel_Kd, 0)               # import the parameters into the PID controller
+        self.steering_PID = PID(steer_Kp, steer_Ki, steer_Kd, 0)            # Set point is 0 here, because later calculations evaluate to 0 when on base.
 
 
-    def update_current_pose(self, msg):
-    #############################################################################################
-    # 
-    #############################################################################################    
+
+    def update_current_pose(self, msg) -> None:
+        #############################################################################################
+        # Updates the current position by taking data from the amcl localization, and extracts usable
+        # data from the output data object.
+        #############################################################################################    
         quaternion = (
 			msg.pose.pose.orientation.x,
 			msg.pose.pose.orientation.y,    # extracting data from the pose
@@ -48,22 +69,62 @@ class MapFollower:
         self.current_pose = [msg.pose.pose.position.x, msg.pose.pose.position.y, yaw]    # Set the current position
 
 
-    def control_loop(self, msg) -> None:
-    #############################################################################################
-    #
-    # 
-    # TODO: Figure out how to run this at 10Hz
-    # 
-    #############################################################################################    
-        
-        # changable params
-        waypoint_tolerance = 20
-        will_loop = True
 
-        # calculating tolerance 
+    def process_steering_angle(self, waypoint_1 : list, waypoint_2 : list, robot_pos : list, vel: float) -> float:
+        #############################################################################################
+        # Takes in the waypoint data, the robot's current position, and an arbitary velocity value, 
+        # and returns a floating point angle for the car to target in radians.
+        # 
+        # 'waypoint_1'      - list format of data extracted from the waypoint file; first target
+        # 'waypoint_2'      - list format of data extracted from the waypoint file; second target
+        # 'robot_pos'       - list format of data gotten from AMCL; robot's current position
+        # 'vel'             - float; arbitary value that determines how quickly the robot levels out to being 'on axis'
+        #
+        # returns steering_angle_local
+        # 'steering_angle_local'    - float, Radians; the calculated steering angle for the rally car to target.
+        #############################################################################################    
+        
+        waypoint_1_tup = (waypoint_1[0], waypoint_1[1])
+        waypoint_2_tup = (waypoint_2[0], waypoint_2[1])                 # converts data to be readable by following methods
+        robot_pos_tup = (self.current_pose[0], self.current_pose[1])
+        
+        p_dist = calculate_perpendicular_dist(waypoint_1_tup, waypoint_2_tup, robot_pos_tup)    # calculates the perpendicular distance
+        theta_transform = calculate_waypoint_angle(waypoint_1_tup, waypoint_2_tup)              # calculates the angle between the two waypoints in global space
+        steering_angle_global = calculate_steering_angle(vel, p_dist, theta_transform)          # calculates a smooth steering angle based on arbitary velocity and previous calculation
+        steering_angle_local = math.radians(self.current_pose[2]) - steering_angle_global       # converts the global steering angle back to local space
+
+        return steering_angle_local
+
+
+
+    def control_loop(self) -> None:
+        #############################################################################################
+        # The "Main" loop of the rallycar control. Makes use incoming data to update the current waypoint,
+        # calculate the heading for the rallycar steering, and then use a PID controller for both
+        # the acceleration and the steering angle. 
+        # 
+        # TODO: Figure out how to run this at 10 / 30 / 50 / variable Hz
+        #           *Probably not included inside the function.
+        # TODO: Messy Comments, need to clean up
+        # 
+        #############################################################################################    
+
+        # TODO: THIS NEEDS TO BE POPULATED BEFORE THIS WORKS!!!!
+        current_vel = None
+        current_steering_angle = None
+        # HACK: current_vel and current_steering_angle are not provided yet! This code stops the interperter from yelling but it does not work yet!
+        vel_error = self.accel_PID(current_vel - self.set_vel)
+        steer_error = self.steering_PID(current_steering_angle - self.process_steering_angle(self.previous_waypoint, self.desired_waypoint, self.current_pose, self.steering_vel))
+
+        # Waypoint params
+        waypoint_tolerance = 20 # defines the radial distance required to "complete" a waypoint
+        will_loop = True        # defines if the robot will target waypoint 0 once it reaches the final waypoint, or terminate
+        # Waypoint params
+
+        # Calculating tolerance 
         dist_to_desired_waypoint = calculate_linear_dist((self.current_pose[0], self.current_pose[1]), (self.desired_waypoint[0], self.desired_waypoint[1]))
         
-        # gateway condition
+        # Gateway condition
         if(dist_to_desired_waypoint > waypoint_tolerance):
             # should the distance to the desired waypoint be greater than the tolerance, exit now.
             return
@@ -72,17 +133,18 @@ class MapFollower:
         if((self.index > len(self.waypoints.pose_arr)) and will_loop == True): 
             # if index is larger than the waypoint array, reset the index back to 0
             # basically, will call a continous looping of the waypoints if true
-            self.index = 0
-
+            self.index = -1 # will have one added to it - making it 0
         elif (self.index > len(self.waypoints.pose_arr) and will_loop == False):
             # HACK: This avoids an indexOutOfBoundsException, but never actually closes down the 
-            #   control loop.
+            #   control loop. While this does not fix the issue, it does tell the car to stop moving forward.
             # TODO: Actually close the control loop.
-            # stops the loop from updating if the end of the array has been reached
+            # stops the loop from updating if the end of the array has been reached            
+            self.set_vel = 0
             return
 
         # Stepping the desired waypoint 
-        self.index += 1
-        self.previous_waypoint = self.desired_waypoint
-        self.desired_waypoint = self.waypoints.pose_arr[self.index]
-    
+        self.index += 1                                             # step the index
+        self.previous_waypoint = self.desired_waypoint              # save the previous desired waypoint
+        self.desired_waypoint = self.waypoints.pose_arr[self.index] # set the new desired waypoint
+
+
