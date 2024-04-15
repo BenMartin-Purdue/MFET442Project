c6e1ffa5e4f65b96e4ab3c46a010ec007a798853
Zachary <139710414+ZachWilson03@users.noreply.github.com>
2024-04-15 08:03:28 -0400

    Added Empty File to be filled
    Title

---
+++ map_follower.py
@@ -0,0 +1,10 @@
+"""
+MFET 44200 - Lab 06
+Contributers: Zachary Wilson
+
+Description: The main file of Lab 06 that controls the acutal movement and navigation of the 
+    rally car based on incoming data from LIDAR, odometry, and AMCL position localization.  
+
+TODO: Add more
+
+"""
